c181b17ecd23ae13d3c3e03ef1024b20e9ae959b
lexxizz <lexxizz@gmail.com>
2017-04-13 22:31:55 +0300

    исправлен календарь

--- resources/views/common.blade.php
+++ resources/views/common.blade.php
@@ -9,10 +9,9 @@
 
     <meta name="_token" content="{{ csrf_token() }}">
     <link href="{!! asset('css/common.css') !!}" media="all" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="https://unpkg.com/flatpickr/dist/flatpickr.min.css">
+    <link href="{!! asset('css/flatpickr.min.css') !!}" media="all" rel="stylesheet" type="text/css" />
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400|PT+Sans:700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700" rel="stylesheet">
-    <script src="https://unpkg.com/flatpickr"></script>
     <script src="https://use.fontawesome.com/a522b0d004.js"></script>
     <script src="{!! asset('js/nouislider.min.js') !!}"></script>
 
@@ -22,7 +21,6 @@
 <div class="body body--popup" id="layout__main"></div>
 
 <script src={{ env('NAME') }}></script>
-<script src="{!! asset('js/scripts.js') !!}"></script>
 
 </body>
 </html>
